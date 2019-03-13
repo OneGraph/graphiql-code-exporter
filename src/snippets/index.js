86fc338df056ac778a79354bc0257048b773a87d
Daniel Woelfel <dwwoelfel@gmail.com>
2019-03-13 11:32:47 -0700

    require necessary prism components

--- src/snippets/index.js
+++ src/snippets/index.js
@@ -2,13 +2,16 @@
 import jsFetch from './javascript/fetch';
 import jsReactApollo from './javascript/reactApollo';
 import jsReactHooks from './javascript/reactHooks';
+require('prismjs/components/javascript');
 
 // ruby
 import rubyGraphqlClient from './ruby/graphqlClient';
+require('prismjs/components/ruby');
 
 // python
 import pythonGraphqlclient from './python/graphqlclient';
 import pythonSgqlc from './python/sgqlc';
+require('prismjs/components/python');
 
 export default [
   jsFetch,
